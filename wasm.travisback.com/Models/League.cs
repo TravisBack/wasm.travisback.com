34cbfcd15a2401ae031f32c9d88f73888ac020e2
Travis Back <travisback@icloud.com>
2024-01-28 20:27:17 -0800

    Added documentation.

--- wasm.travisback.com/Models/League.cs
+++ wasm.travisback.com/Models/League.cs
@@ -1,18 +1,59 @@
-﻿namespace wasm.travisback.com.Models
+﻿//------------------------------------------------------------------------------
+// File: Leagues.cs
+//
+// Description: This file contains the LeagueInfoAttribute class, which is a 
+//              custom attribute providing additional information about a sports league.
+//              It also includes the League enum representing various sports leagues.
+//
+// Author: Travis Back
+//
+// Created: 2024-1-28
+//
+// Last Modified: 2024-1-28
+//
+// Notes:
+// - LeagueInfoAttribute encapsulates the description and URL information for a league.
+// - League enum includes entries for different sports leagues with associated attributes.
+//------------------------------------------------------------------------------
+
+using System;
+
+namespace wasm.travisback.com.Models
 {
+    /// <summary>
+    /// Custom attribute to provide additional information about a league.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     sealed class LeagueInfoAttribute : Attribute
     {
         private static string BaseURL = "https://site.api.espn.com/apis/site/v2/sports";
+
+        /// <summary>
+        /// Gets the description of the league.
+        /// </summary>
         public string Description { get; }
+
+        /// <summary>
+        /// Gets the relative URL of the league.
+        /// </summary>
         public string LeaugeURL { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the LeagueInfoAttribute class.
+        /// </summary>
+        /// <param name="description">The description of the league.</param>
+        /// <param name="leaugeURL">The relative URL of the league.</param>
         public LeagueInfoAttribute(string description, string leaugeURL)
         {
             Description = description;
             LeaugeURL = leaugeURL;
         }
 
+        /// <summary>
+        /// Gets the description of the specified league.
+        /// </summary>
+        /// <param name="league">The league for which to retrieve the description.</param>
+        /// <returns>The description of the league.</returns>
         public static string GetLeagueDescription(League league)
         {
             var field = league.GetType().GetField(league.ToString());
@@ -20,6 +61,11 @@
             return attribute == null ? string.Empty : attribute.Description;
         }
 
+        /// <summary>
+        /// Gets the full URL of the specified league.
+        /// </summary>
+        /// <param name="league">The league for which to retrieve the URL.</param>
+        /// <returns>The full URL of the league.</returns>
         public static string GetLeagueURL(League league)
         {
             var field = league.GetType().GetField(league.ToString());
@@ -28,21 +74,42 @@
         }
     }
 
+    /// <summary>
+    /// Enum representing various sports leagues.
+    /// </summary>
     public enum League
     {
-        [LeagueInfo("Major League Baseball","/baseball/mlb")]
+        [LeagueInfo("English Premier League", "/soccer/eng.1")]
+        EPL,
+
+        [LeagueInfo("Major League Baseball", "/baseball/mlb")]
         MLB,
 
-        [LeagueInfo("Major League Soccer", "/soccer/mls")]
+        [LeagueInfo("Major League Soccer", "/soccer/usa.1")]
         MLS,
 
         [LeagueInfo("National Basketball Association", "/basketball/nba")]
         NBA,
 
+        [LeagueInfo("NCAA Baseball", "/baseball/college-baseball")]
+        NCAABaseball,
+
+        [LeagueInfo("NCAA Basketball", "/basketball/mens-college-basketball")]
+        NCAABasketball,
+
+        [LeagueInfo("NCAA Football", "/football/college-football")]
+        NCAAFootball,
+
+        [LeagueInfo("NCAA Women's Basketball", "/basketball/womens-college-basketball")]
+        NCAAWomensBasketball,
+
         [LeagueInfo("National Football League", "/football/nfl")]
         NFL,
 
         [LeagueInfo("National Hockey League", "/hockey/nhl")]
-        NHL
+        NHL,
+
+        [LeagueInfo("Women's National Basketball Association", "/basketball/wnba")]
+        WNBA,
     }
 }
--- wasm.travisback.com/Services/ApiService.cs
+++ wasm.travisback.com/Services/ApiService.cs
@@ -1,36 +1,67 @@
-﻿using System.Runtime.CompilerServices;
+﻿//------------------------------------------------------------------------------
+// File: ApiService.cs
+//
+// Description: This file contains the ApiService class representing a service
+//              for interacting with an external API. It includes methods to 
+//              retrieve data from the API.
+//
+// Author: Travis Back
+//
+// Created: 2024-1-28
+//
+// Last Modified: 2024-1-28
+//
+// Notes:
+// - This service class uses HttpClient to make asynchronous requests to the API.
+//------------------------------------------------------------------------------
+
 using wasm.travisback.com.Models;
 
 namespace wasm.travisback.com.Services
 {
+    /// <summary>
+    /// Service class for interacting with the API.
+    /// </summary>
     public class ApiService
     {
         private readonly IConfiguration _configuration;
-
         private readonly string _baseApiURL;
-        
         private HttpClient httpClient = new HttpClient();
 
+        /// <summary>
+        /// Initializes a new instance of the ApiService class.
+        /// </summary>
+        /// <param name="configuration">The configuration to retrieve settings.</param>
         public ApiService(IConfiguration configuration)
         {
             _configuration = configuration;
-
-            
         }
 
+        /// <summary>
+        /// Gets the scoreboard for the specified league.
+        /// </summary>
+        /// <param name="league">The league for which to retrieve the scoreboard.</param>
+        /// <returns>A string representing the scoreboard data.</returns>
         public async Task<string> GetScoreBoard(League league)
         {
+            // Construct the URL for retrieving the scoreboard based on the league.
             var scoreURL = _baseApiURL + LeagueInfoAttribute.GetLeagueURL(league) + "/scoreboard";
 
             try
             {
+                // Make an asynchronous GET request to the API.
                 var response = await httpClient.GetAsync(scoreURL);
+
+                // Ensure the response indicates success.
                 response.EnsureSuccessStatusCode();
+
+                // Read and return the response content.
                 var responseContent = await response.Content.ReadAsStringAsync();
                 return responseContent;
             }
             catch (Exception ex)
             {
+                // Handle exceptions and return an error message.
                 Console.WriteLine(ex);
                 return ex.Message;
             }
